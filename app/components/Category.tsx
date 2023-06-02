c27631bd5efc67af4f3c3a40bf9dffbc9ab59b45
Jordan <jordanroberts333@icloud.com>
2023-06-02 22:01:45 +0100

    small timing

--- app/components/Category.tsx
+++ app/components/Category.tsx
@@ -36,7 +36,7 @@ const Category = ({title, imageUrl}: Props) => {
     return (
         <>
             <div ref={imageWrapperRef} className='relative w-full aspect-[3/2] overflow-hidden'>
-                <Image ref={imageRef} src={imageUrl} alt='placeholder' fill className='object-cover will-change-transform'/>
+                <Image ref={imageRef} src={imageUrl} alt='placeholder' fill className='object-cover will-change-transform duration-[5ms]'/>
             </div>
         </>
     )
